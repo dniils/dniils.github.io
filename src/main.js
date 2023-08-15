f968df58943f388c54d1f43a0b8afd5d5e7fef47
Daniil Shitikov <tochka098@gmail.com>
2023-08-15 20:44:47 +0400

    feat: get data from github & implement vue's list rendering

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/style/main.scss";
+import { createPinia } from "pinia";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(router).mount("#app");
